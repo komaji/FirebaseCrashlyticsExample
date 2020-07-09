c654789e7dcb753b9087f0ced8c3b43d00ad9c82
kojikoji <kojima.t@pepabo.com>
2020-07-09 11:28:33 +0900

    git init

---
+++ FirebaseCrashlyticsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseCrashlyticsExample
+//
+//  Created by kojima.t on 2020/07/09.
+//  Copyright © 2020 kojima.t. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
